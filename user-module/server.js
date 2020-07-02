f847dd5eb5421fa70149d267d76a622791603d0f
Nicholas Iatropoulos <nicholasiatropoulos@hotmail.com>
2020-07-02 17:50:49 -0400

    added passport, cookieParser, bodyParser, session

--- user-module/server.js
+++ user-module/server.js
@@ -1,3 +1,7 @@
 var express = require('express');
 var app = express();
 var port = 3001;
+var passport = require('passport');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
