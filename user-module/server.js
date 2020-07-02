195f291c178acc0d8d364d6ca9a56e1e44435793
Nicholas Iatropoulos <nicholasiatropoulos@hotmail.com>
2020-07-02 18:30:49 -0400

    removed cookie-parser

--- user-module/server.js
+++ user-module/server.js
@@ -2,14 +2,18 @@ var express = require('express');
 var app = express();
 var port = 3001;
 var passport = require('passport');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 
-app.use(cookieParser());
 app.use(bodyParser());
 
-app.use(session({ secret: 'mySupErSecReTSECreT!!' }));
+app.use(
+  session({
+    secret: 'mySupErSecReTSECreT!!',
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
