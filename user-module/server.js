7839274ee593ca2d0561861a3a77e667e7b98b91
Nicholas Iatropoulos <nicholasiatropoulos@hotmail.com>
2020-07-02 18:03:32 -0400

    app will now use cookieParser, bodyParser and passport

--- user-module/server.js
+++ user-module/server.js
@@ -5,3 +5,10 @@ var passport = require('passport');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
+
+app.use(cookieParser());
+app.use(bodyParser());
+
+app.use(session({ secret: 'mySupErSecReTSECreT!!' }));
+app.use(passport.initialize());
+app.use(passport.session());
