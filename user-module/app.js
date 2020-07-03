2ae37b04d4a954fd227f12cd34c8f37834e18e1c
Nicholas Iatropoulos <nicholasiatropoulos@hotmail.com>
2020-07-03 11:43:53 -0400

    added pug templates and routes

--- user-module/app.js
+++ user-module/app.js
@@ -13,6 +13,17 @@ connection.connect();
 
 var app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+  res.render('index', { title: 'User Management' });
+});
+
+app.get('/register', (req, res) => {
+  res.render('create_user', { title: 'Create User' });
+});
+
 app.listen(3001, () => {
   console.log('Server started on port 3001');
 });
