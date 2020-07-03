b4393bf5e2ea8cdd352251ebdb7a602f6d7e9435
Nicholas Iatropoulos <nicholasiatropoulos@hotmail.com>
2020-07-03 12:02:53 -0400

    added user login and update templates and routes

--- user-module/app.js
+++ user-module/app.js
@@ -24,6 +24,14 @@ app.get('/register', (req, res) => {
   res.render('create_user', { title: 'Create User' });
 });
 
+app.get('/login', (req, res) => {
+  res.render('user_login', { title: 'Login' });
+});
+
+app.get('/update', (req, res) => {
+  res.render('update_user', { title: 'Update User Information' });
+});
+
 app.listen(3001, () => {
   console.log('Server started on port 3001');
 });
