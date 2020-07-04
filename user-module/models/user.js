ccdc9b4df3b1282e8470c69bed273798057b9650
Nicholas Iatropoulos <nicholasiatropoulos@hotmail.com>
2020-07-04 01:52:24 -0400

    added insertUser function to user.js

--- user-module/models/user.js
+++ user-module/models/user.js
@@ -1,1 +1,51 @@
+var bodyParser = require('body-parser');
+var database = require('../config/database');
 
+exports.insertUser = (req, res) => {
+  var sql =
+    'INSERT INTO users VALUES (' +
+    "'" +
+    req.body.username +
+    "'" +
+    ', ' +
+    "'" +
+    req.body.password +
+    "'" +
+    ', ' +
+    "'" +
+    req.body.first_name +
+    "'" +
+    ', ' +
+    "'" +
+    req.body.last_name +
+    "'" +
+    ', ' +
+    "'" +
+    req.body.email +
+    "'" +
+    ', ' +
+    "'" +
+    req.body.street +
+    "'" +
+    ', ' +
+    "'" +
+    req.body.city +
+    "'" +
+    ', ' +
+    "'" +
+    req.body.province +
+    "'" +
+    ', ' +
+    "'" +
+    req.body.postal_code +
+    "'" +
+    ');';
+  database.connection.query(sql, (err, result) => {
+    if (err) {
+      console.log(err);
+      return;
+    } else {
+      return;
+    }
+  });
+};
