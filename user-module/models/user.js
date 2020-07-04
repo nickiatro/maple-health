71bc68115b2e4753bcb71f7ab11156a626edaac6
Nicholas Iatropoulos <nicholasiatropoulos@hotmail.com>
2020-07-04 14:48:08 -0400

    updated sql in user.js

--- user-module/models/user.js
+++ user-module/models/user.js
@@ -4,41 +4,23 @@ var database = require('../config/database');
 exports.insertUser = (req, res) => {
   var sql =
     'INSERT INTO users VALUES (' +
-    "'" +
-    req.body.username +
-    "'" +
+    (req.body.username ? "'" + req.body.username + "'" : 'NULL') +
     ', ' +
-    "'" +
-    req.body.password +
-    "'" +
+    (req.body.password ? "'" + req.body.password + "'" : 'NULL') +
     ', ' +
-    "'" +
-    req.body.first_name +
-    "'" +
+    (req.body.first_name ? "'" + req.body.first_name + "'" : 'NULL') +
     ', ' +
-    "'" +
-    req.body.last_name +
-    "'" +
+    (req.body.last_name ? "'" + req.body.last_name + "'" : 'NULL') +
     ', ' +
-    "'" +
-    req.body.email +
-    "'" +
+    (req.body.email ? "'" + req.body.email + "'" : 'NULL') +
     ', ' +
-    "'" +
-    req.body.street +
-    "'" +
+    (req.body.street ? "'" + req.body.street + "'" : 'NULL') +
     ', ' +
-    "'" +
-    req.body.city +
-    "'" +
+    (req.body.city ? "'" + req.body.city + "'" : 'NULL') +
     ', ' +
-    "'" +
-    req.body.province +
-    "'" +
+    (req.body.province ? "'" + req.body.province + "'" : 'NULL') +
     ', ' +
-    "'" +
-    req.body.postal_code +
-    "'" +
+    (req.body.postal_code ? "'" + req.body.postal_code + "'" : 'NULL') +
     ');';
   database.connection.query(sql, (err, result) => {
     if (err) {
