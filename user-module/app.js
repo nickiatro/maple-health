b0c4b3a585b4d0489acb5c2da5ce4ecab80715c8
Nicholas Iatropoulos <nicholasiatropoulos@hotmail.com>
2020-07-03 23:27:14 -0400

    created database.js

--- user-module/app.js
+++ user-module/app.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var path = require('path');
-var mysql = require('mysql');
+var user = require('./models/user');
+var bodyParser = require('body-parser');
+var database = require('./config/database');
 
 var app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -10,26 +15,7 @@ app.set('view engine', 'pug');
 require('./routes/routes')(app);
 require('./routes/user_routes')(app);
 
-var connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'mtlqcH3G1M8ca',
-  database: 'company',
-});
-
-connection.connect(err => {
-  if (err) throw err;
-  var sql = 'DROP TABLE IF EXISTS users';
-  connection.query(sql, (err, result) => {
-    if (err) throw err;
-  });
-  sql =
-    'CREATE TABLE users (username VARCHAR(255) PRIMARY KEY, password VARCHAR(255) NOT NULL, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL ,email VARCHAR(255) NOT NULL, street VARCHAR(255) NOT NULL, city VARCHAR(255) NOT NULL, province VARCHAR(255) NOT NULL, postal_code VARCHAR(7) NOT NULL)';
-  connection.query(sql, (err, result) => {
-    if (err) throw err;
-  });
-  connection.end();
-});
+database.createTables();
 
 app.listen(3001, () => {
   console.log('Server started on port 3001');
