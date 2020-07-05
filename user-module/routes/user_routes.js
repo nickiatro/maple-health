8b9ad57044844c2a5086ed54abf1ad9482487a3b
Nicholas Iatropoulos <nicholasiatropoulos@hotmail.com>
2020-07-05 01:54:23 -0400

    added route to user_routes.js

--- user-module/routes/user_routes.js
+++ user-module/routes/user_routes.js
@@ -1,4 +1,11 @@
 var user_controller = require('../controllers/user_controller');
 module.exports = app => {
   app.post('/register', user_controller.insertUser);
+
+  app.get('/update', (req, res) => {
+    res.render('update_user', {
+      title: 'Update User Information',
+      result: user_controller.getAllUsers(),
+    });
+  });
 };
