ba10f3fea3adcf8abe60b9d232d26e2db5d95ecb
Nicholas Iatropoulos <nicholasiatropoulos@hotmail.com>
2020-07-05 01:50:08 -0400

    added getAllUsers function to user_controller.js

--- user-module/controllers/user_controller.js
+++ user-module/controllers/user_controller.js
@@ -2,6 +2,10 @@ var User = require('../models/user');
 
 exports.insertUser = (req, res) => {
   User.insertUser(req, res);
-  res.redirect('/register');
+  res.redirect('/');
   return;
+};
+
+exports.getAllUsers = (req, res) => {
+  return User.getAllUsers(req, res);
 };
