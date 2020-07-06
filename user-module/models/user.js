f6197e025ea3e92abfd0d59bf32af8ca69e65a15
Nicholas Iatropoulos <nicholasiatropoulos@hotmail.com>
2020-07-06 00:47:46 -0400

    added password hashing to user.js

--- user-module/models/user.js
+++ user-module/models/user.js
@@ -1,34 +1,47 @@
 var bodyParser = require('body-parser');
 var database = require('../config/database');
+var bcrypt = require('bcryptjs');
 
 exports.insertUser = (req, res) => {
-  var sql =
-    'INSERT INTO users VALUES (' +
-    (req.body.username ? "'" + req.body.username + "'" : 'NULL') +
-    ', ' +
-    (req.body.password ? "'" + req.body.password + "'" : 'NULL') +
-    ', ' +
-    (req.body.first_name ? "'" + req.body.first_name + "'" : 'NULL') +
-    ', ' +
-    (req.body.last_name ? "'" + req.body.last_name + "'" : 'NULL') +
-    ', ' +
-    (req.body.email ? "'" + req.body.email + "'" : 'NULL') +
-    ', ' +
-    (req.body.street ? "'" + req.body.street + "'" : 'NULL') +
-    ', ' +
-    (req.body.city ? "'" + req.body.city + "'" : 'NULL') +
-    ', ' +
-    (req.body.province ? "'" + req.body.province + "'" : 'NULL') +
-    ', ' +
-    (req.body.postal_code ? "'" + req.body.postal_code + "'" : 'NULL') +
-    ');';
-  database.connection.query(sql, (err, result) => {
-    if (err) {
-      throw err;
-    } else {
-      return;
-    }
-  });
+  if (req.body.password) {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(req.body.password, salt, (err, hash) => {
+        if (err) {
+          throw err;
+        }
+        insertWithHash(hash);
+      });
+    });
+  }
+  const insertWithHash = hash => {
+    var sql =
+      'INSERT INTO users VALUES (' +
+      (req.body.username ? "'" + req.body.username + "'" : 'NULL') +
+      ', ' +
+      (req.body.password ? "'" + hash + "'" : 'NULL') +
+      ', ' +
+      (req.body.first_name ? "'" + req.body.first_name + "'" : 'NULL') +
+      ', ' +
+      (req.body.last_name ? "'" + req.body.last_name + "'" : 'NULL') +
+      ', ' +
+      (req.body.email ? "'" + req.body.email + "'" : 'NULL') +
+      ', ' +
+      (req.body.street ? "'" + req.body.street + "'" : 'NULL') +
+      ', ' +
+      (req.body.city ? "'" + req.body.city + "'" : 'NULL') +
+      ', ' +
+      (req.body.province ? "'" + req.body.province + "'" : 'NULL') +
+      ', ' +
+      (req.body.postal_code ? "'" + req.body.postal_code + "'" : 'NULL') +
+      ');';
+    database.connection.query(sql, (err, result) => {
+      if (err) {
+        throw err;
+      } else {
+        return;
+      }
+    });
+  };
 };
 
 var results;
