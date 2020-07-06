cf686023101f4957c639744828e9338812df0197
Nicholas Iatropoulos <nicholasiatropoulos@hotmail.com>
2020-07-06 00:41:57 -0400

    added an sql file to insert sample data

---
+++ user-module/public/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO users VALUES ('JonnyStaub','password123','Jonny','Staub','jonny.staub@hotmail.com','500-969 Robson St','Vancouver','BC','V6Z 1X5');
+INSERT INTO users VALUES ('HollyConway','password123','Holly','Conway','holly.conway@hotmail.com','500-969 Robson St','Vancouver','BC','V6Z 1X5');
+INSERT INTO users VALUES ('NiraArora','password123','Nira','Arora','nira.arora@hotmail.com','500-969 Robson St','Vancouver','BC','V6Z 1X5');
+INSERT INTO users VALUES ('AmyBeeman','password123','Amy','Beeman','amy.beeman@hotmail.com','500-969 Robson St','Vancouver','BC','V6Z 1X5');
+INSERT INTO users VALUES ('VinnyBarrucco','password123','Vinny','Barrucco','vinny.barrucco@hotmail.com','1717 Boul René-Lévesque E','Montréal','QC','H2L 4T9');
+INSERT INTO users VALUES ('ShannonKing','password123','Shannon','King','shannon.king@hotmail.com','1717 Boul René-Lévesque E','Montréal','QC','H2L 4T9');
+INSERT INTO users VALUES ('AdamGreenburg','password123','Adam','Greenburg','adam.greenburg@hotmail.com','1717 Boul René-Lévesque E','Montréal','QC','H2L 4T9');
+INSERT INTO users VALUES ('LeeHaberkorn','password123','Lee','Haberkorn','lee.haberkorn@hotmail.com','1717 Boul René-Lévesque E','Montréal','QC','H2L 4T9');
