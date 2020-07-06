7cb70219f34f33a7e09becead560d6f7e85c4b08
Nicholas Iatropoulos <nicholasiatropoulos@hotmail.com>
2020-07-06 16:17:10 -0400

    fixed login issue

--- user-module/app.js
+++ user-module/app.js
@@ -7,9 +7,13 @@ var passport = require('passport');
 
 var app = express();
 
+require('./config/passport')(passport);
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -18,10 +22,6 @@ require('./routes/routes')(app);
 require('./routes/user_routes')(app);
 
 database.createUserTable();
-
-require('./config/passport')(passport);
-app.use(passport.initialize());
-app.use(passport.session());
 
 app.listen(3001, () => {
   console.log('Server started on port 3001');
--- user-module/config/passport.js
+++ user-module/config/passport.js
@@ -11,14 +11,13 @@ module.exports = function(passport) {
         "SELECT * FROM users WHERE username ='" + username + "'",
         function(err, user) {
           if (err) throw err;
-          if (!user) {
+          if (user.length == 0) {
             return done(null, false, { message: 'No user found!' });
           }
-
           bcrypt.compare(password, user[0].password, function(err, isMatch) {
             if (err) throw err;
             if (isMatch) {
-              return done(null, user);
+              return done(null, user[0]);
             } else {
               return done(null, false, { message: 'Incorrect password!' });
             }
@@ -29,12 +28,15 @@ module.exports = function(passport) {
   );
 
   passport.serializeUser(function(user, done) {
-    done(null, user.id);
+    done(null, user.username);
   });
 
-  passport.deserializeUser(function(id, done) {
-    User.findById(id, function(err, user) {
-      done(err, user);
-    });
+  passport.deserializeUser(function(username, done) {
+    database.connection.query(
+      "SELECT * FROM users WHERE username = '" + username + "'",
+      function(err, user) {
+        done(err, user[0]);
+      }
+    );
   });
 };
