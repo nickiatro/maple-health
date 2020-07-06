f9a831ed080db3a88bbfa63d1a9879de36202c2e
Nicholas Iatropoulos <nicholasiatropoulos@hotmail.com>
2020-07-06 02:36:52 -0400

    created passport.js

---
+++ user-module/config/passport.js
@@ -0,0 +1,40 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('../models/user');
+const database = require('../config/database');
+const bcrypt = require('bcryptjs');
+
+module.exports = function(passport) {
+  passport.use(
+    new LocalStrategy(function(username, password, done) {
+      database.connection.query(
+        "SELECT * FROM users WHERE username ='" + username + "'",
+        function(err, user) {
+          if (err) throw err;
+          if (!user) {
+            return done(null, false, { message: 'No user found!' });
+          }
+
+          bcrypt.compare(password, user[0].password, function(err, isMatch) {
+            if (err) throw err;
+            if (isMatch) {
+              return done(null, user);
+            } else {
+              return done(null, false, { message: 'Incorrect password!' });
+            }
+          });
+        }
+      );
+    })
+  );
+
+  passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+  });
+};
