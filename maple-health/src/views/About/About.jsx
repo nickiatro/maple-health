ec011b34eb1f3ef60ab6f42c836509a4f787d559
Nicholas Iatropoulos <nicholasiatropoulos@hotmail.com>
2020-07-13 13:02:31 -0400

    updated about page

--- maple-health/src/views/About/About.jsx
+++ maple-health/src/views/About/About.jsx
@@ -3,7 +3,34 @@ const About = () => {
   return (
     <div>
       <br />
-      <h1>About</h1>
+      <h1>Your Health Questions. Answered.</h1>
+      <h6>
+        Maple Health Montréal has a dedicated team of doctors and nurses willing
+        to answer
+        <br /> all questions regarding your health and wellness.
+      </h6>
+      <br />
+      <h1>
+        Bonjour! Hello!
+        <br /> We speak your language.
+      </h1>
+      <h6>
+        All of our team is fluently bilingual in English and French. <br />
+        We proudly offer our services in your language of choice!
+      </h6>
+      <br />
+      <h1>We're just around the corner!</h1>
+      <h6>
+        We understand Greater Montréal because we live here, too. <br /> We
+        serve people across the Communauté métropolitaine de Montréal.
+      </h6>
+      <br />
+      <h1>
+        Tired of waiting on hold?
+        <br />
+        We get that!
+      </h1>
+      <h6>That's why all questions are answered via email.</h6>
     </div>
   );
 };
